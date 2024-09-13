ee1915e3ca7ad70d77313c4ee2042d0396de4d0f
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-09-13 16:05:43 +0200

    fixing things

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import ImdbController from "./imdb_controller"
-application.register("imdb-id", ImdbController)
 
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
