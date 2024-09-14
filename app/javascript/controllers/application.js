e8410cd53cf1a2700dcfefc8140b458dfa0a185e
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-09-14 07:47:21 +0200

    changed form submission from local to remote

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,8 @@
 import { Application } from "@hotwired/stimulus";
-import ImdbController from "./imdb_controller";
+// import ImdbController from "./imdb_controller";
 
 const application = Application.start();
-application.register("imdb", ImdbController);
+// application.register("imdb", ImdbController);
 
 window.Stimulus = application;
 export { application };
