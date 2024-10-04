be94b1792bdb5c52bf6fc92ed1fbede53f8f9c42
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-10-04 15:51:30 +0200

    code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
-# require 'rspotify/oauth'
+require 'rspotify/oauth'
+require 'rspotify'
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :spotify, ENV["spotify_client_id"], ENV["spotify_client_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-# end
 
-# OmniAuth.config.allowed_request_methods = [ :post, :get ]
+RSpotify.authenticate(ENV["spotify_client_id"], ENV["spotify_client_secret"])
 
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify, ENV["spotify_client_id"], ENV["spotify_client_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+end
+
+OmniAuth.config.allowed_request_methods = [ :post, :get ]
