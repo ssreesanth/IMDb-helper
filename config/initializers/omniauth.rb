d080139d6250463a7de14765fc45611abe8452eb
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-10-04 09:19:41 +0200

    removed secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,8 +7,5 @@ end
 OmniAuth.config.allowed_request_methods = [ :post, :get ]
 
 
-
-# RSpotify.authenticate("9cc2d3bdbdf146cbbd2e80b219239dc3", "3d207e6433444d8581ffffe10727c11f")
 # user = RSpotify::User.find("1193316999")
 # new_playlist = user.create_playlist!('Test Playlist IRB')
-
