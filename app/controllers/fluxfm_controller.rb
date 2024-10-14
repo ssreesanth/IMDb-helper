1c628a51e668e3976dc469ebd172dc222a64e047
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-10-14 11:06:38 +0200

    refactored controller and added advise

---
+++ app/controllers/fluxfm_controller.rb
@@ -0,0 +1,21 @@
+class FluxfmController < ApplicationController
+
+  def index
+    date = params[:date].presence || Date.today
+    url = "https://archiv.fluxfm.de/fluxfm-playlist/?loc=berlin&date=#{date}&ajax=1&days=30&limit=30"
+    puts "Generated URL: #{url}"
+    html = URI.open(url)
+    doc = Nokogiri::HTML.parse(html)
+    @songs = doc.search(".title")
+
+
+    # @user = RSpotify::User.find("1193316999")
+    # playlist = @user.create_playlist!('FluxFm Top 30')
+    # songs.each do |song|
+    #   # Search for the song on Spotify
+    #   tracks = RSpotify::Track.search(song)
+    #   playlist.add_tracks!(tracks)
+    # end
+  end
+
+end
---
+++ app/controllers/nasa_controller.rb
@@ -0,0 +1,11 @@
+class NasaController < ApplicationController
+
+  def index
+    api_key = ENV["NASA_API"]
+    date = params[:date].presence || Date.today
+    url = "https://api.nasa.gov/planetary/apod?api_key=#{api_key}&start_date=#{date}"
+    image_serialized = URI.parse(url).read
+    @image = JSON.parse(image_serialized)
+  end
+
+end
